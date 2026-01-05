ae07947f25b3a5bf425433c89affee4fcdb207bd
tomac <5379823@qq.com>
2026-01-05 23:10:18 +0800

    rm useless

--- sources/com/example/hilogemail/R$mipmap.java
+++
@@ -1,9 +0,0 @@
-package com.example.hilogemail;
-
-/* loaded from: classes.dex */
-public final class R$mipmap {
-    public static final int fastview_sdk_ic_cfu_cancel_normal = 2131820544;
-
-    private R$mipmap() {
-    }
-}
